3452fc6e70601375b70ba256b38dba9a071cf770
Jorge Luis <cokimaniac@gmail.com>
2019-08-03 18:42:52 -0400

    Updates

---
+++ precache-manifest.61862b5158b0247352c6a6667e4bd8a8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79525cdc43a28f99bdf4f715c2480ea2",
+    "url": "/cv-maker/index.html"
+  },
+  {
+    "revision": "7dcf30e93727dce1cc2b",
+    "url": "/cv-maker/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "922106bc6b9a989720bc",
+    "url": "/cv-maker/static/css/main.92cd3473.chunk.css"
+  },
+  {
+    "revision": "7dcf30e93727dce1cc2b",
+    "url": "/cv-maker/static/js/2.aeb5738c.chunk.js"
+  },
+  {
+    "revision": "922106bc6b9a989720bc",
+    "url": "/cv-maker/static/js/main.d861d796.chunk.js"
+  },
+  {
+    "revision": "db7c18f7546cc5ca240b",
+    "url": "/cv-maker/static/js/runtime~main.3a10a264.js"
+  },
+  {
+    "revision": "b95eaade536c9eff36499f6c2fa49b3f",
+    "url": "/cv-maker/static/media/courses.b95eaade.png"
+  },
+  {
+    "revision": "3bad00339b88cddd7e1eefbf3e9eab4d",
+    "url": "/cv-maker/static/media/education.3bad0033.svg"
+  },
+  {
+    "revision": "57afe7f000eb50b2c06bea0c04c7183f",
+    "url": "/cv-maker/static/media/github-logo.57afe7f0.png"
+  },
+  {
+    "revision": "60a09af7e3a12ed60b5fcdaccfacb2c9",
+    "url": "/cv-maker/static/media/skills.60a09af7.png"
+  },
+  {
+    "revision": "bbc9e187daeaf2465203f7f002b2fb39",
+    "url": "/cv-maker/static/media/twitter-logo.bbc9e187.png"
+  },
+  {
+    "revision": "2fd34f74f42977df5f00ff50222b916c",
+    "url": "/cv-maker/static/media/wordpress-logo.2fd34f74.png"
+  }
+]);
